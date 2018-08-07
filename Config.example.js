2d2a88712d1a410026d031494c8871fba7941ca1
Omer <eldaromer@gmail.com>
2018-08-07 13:05:48 -0600

    Changed gitignore and added example config

---
+++ Config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT: 6868,
+    STRIPE_SECRET: 'sk_live_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    ENDPOINT_SECRET: 'whsec_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    mail: {
+        from: 'Me',
+        fromEmail: 'mymail@mail.com'
+    }
+};
