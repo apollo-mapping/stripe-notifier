9e22f769c2684b05a4a9113929be2be846a1a412
Omer <eldaromer@gmail.com>
2018-08-07 12:19:10 -0600

    Making email content

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ router.post('/hook', (ctx, next) => {
     try {
         let event = stripe.webhooks.constructEvent(ctx.request.rawBody, sig, Config.ENDPOINT_SECRET)
         console.log(event);
+        if (event.type === 'charge.succeeded') {
+            let data = event.data.object;
+            console.log(data);
+        } else {
+
+        }
     } catch (e) {
         ctx.throw(400, e);
     }
@@ -30,10 +36,24 @@ router.post('/hook', (ctx, next) => {
     }
 });
 
+let makeSuccessEmail = (name, amount, invoice, cardType, lastFour, owner, date, company, description) => {
+    return "<p>Dear " + name + "</p>\n\n<p>Here is the receipt for your payment of <b>$" + amount + "</b>" +
+        " referencing quote/invoice number <b>" + invoice + "</b> charged to a <b>" + cardType + "</b> card " +
+        "ending in <b>" + lastFour + "</b> and belonging to <b>" + owner + "</b> on <b>" + date + "</b>.</p>\n\n" +
+        "<p>The following company name and description (if entered) goes along with your charge: " +
+        "<b>" + company + "</b>; <b>" + description + "</b>.</p>\n\n" +
+        "<p>We appreciate your patronage and look forward to working with you in the future!</p>\n\n" +
+        "<p>-- The Apollo Mapping Team</p>";
+};
+
+let makeErrorEmail = () => {
+
+};
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 
 app.listen(Config.PORT, () => {
     console.log('Running on port: ' + Config.PORT);
-})
+});
