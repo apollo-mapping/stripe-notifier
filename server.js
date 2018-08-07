91771051b59a06f974778110504d1e46c4c534d5
Omer <eldaromer@gmail.com>
2018-08-07 12:40:44 -0600

    Made error email

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ router.post('/hook', (ctx, next) => {
 
     try {
         let event = stripe.webhooks.constructEvent(ctx.request.rawBody, sig, Config.ENDPOINT_SECRET);
+        let data = event.data.object;
         if (event.type === 'charge.succeeded') {
-            let today = new Date();
-            let data = event.data.object;
             let email = makeSuccessEmail(data);
             console.log(email);
         } else {
-
+            let email = makeErrorEmail(data);
+            console.log(email);
         }
     } catch (e) {
         ctx.throw(400, e);
@@ -49,8 +49,18 @@ let makeSuccessEmail = (data) => {
         "<p>-- The Apollo Mapping Team</p>";
 };
 
-let makeErrorEmail = () => {
-
+let makeErrorEmail = (data) => {
+    let today = new Date();
+    let date = today.getMonth()+1 + '/' + today.getDate() + '/' + today.getFullYear();
+    return "<p>Dear " + data.metadata.customer_name + ",</p>\n\n" +
+        "<p>There was a failed charge to your credit card!</p>\n\n" +
+        "<p>Specifically, it was a failed payment of <b>$" + data.amount + "</b> referencing quite/invoice number " +
+        "<b>" + data.metadata['Quote/Invoice #'] + "</b> charged to a <b>\" + data.card.brand + \"</b> card  ending " +
+        "in <b>" + data.card.last4 + "</b> and belonging to <b>\" + data.card.name + \"</b> on <b>\" + date + \"</b>.</p>\n\n" +
+        "<p>The following company name and description (if entered) goes along with your failed charge: " +
+        "<b>" + data.metadata['Company Name'] + "</b>; <b>" + data.metadata.Description + "</b>.</p>\n\n" +
+        "<p>And this is the charge failure  message: <b>" + data.failure_message + "</b></p>\n\n" +
+        "<p>-- The Apollo Mapping Team</p>";
 };
 
 app.use(router.routes());
