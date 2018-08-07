0235436d40aaaaeaee6794d7a0585e391f8c0120
Omer <eldaromer@gmail.com>
2018-08-07 12:03:23 -0600

    Signature verification

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Router = require('koa-router');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const Config = require('./Config');
+const stripe = require('stripe')(Config.STRIPE_SECRET);
 const {google} = require('googleapis');
 
 const app = new Koa();
@@ -14,6 +15,15 @@ app.use(bodyParser());
 app.use(cors());
 
 router.post('/hook', (ctx, next) => {
+    let sig = ctx.request.headers['stripe-signature'];
+
+    try {
+        let event = stripe.webhooks.constructEvent(ctx.request.rawBody, sig, Config.ENDPOINT_SECRET)
+        console.log(event);
+    } catch (e) {
+        ctx.throw(400, e);
+    }
+
     console.log(ctx.request.body.data.object);
 
     ctx.status = 200;
