63b33d3cbc7bbae49eeaaa4d910bcef7744b311f
Omer <eldaromer@gmail.com>
2018-08-07 11:38:41 -0600

    Simple testing hook

---
+++ server.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const Router = require('koa-router');
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser');
+const Config = require('./Config');
+const {google} = require('googleapis');
+
+const app = new Koa();
+let router = new Router();
+
+app.use(logger());
+app.use(bodyParser());
+app.use(cors());
+
+router.post('/hook', (ctx, next) => {
+    console.log(ctx.request.body);
+
+    ctx.status = 200;
+    ctx.body = {
+        message: 'Mamma mia'
+    }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+
+app.listen(Config.PORT, () => {
+    console.log('Running on port: ' + Config.PORT);
+})
